2d972c58cdb971d0df42a207a80688605a50c7f9
Daniel Martínez Abellán <2903602@alu365.murciaeduca.es>
2026-09-23 14:20:10 +0200

    subida saludo2

---
+++ saludo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Ejemplo Servidor</title>
+</head>
+<body>
+    <!-- Bloque o sentencia de guión embebido en PHP -->
+    <h1>
+        <?php 
+            $usuario = "Carlos";
+            echo "Bienvenido a la web, " . $usuario; 
+        ?>
+    </h1>
+</body>
+</html>
